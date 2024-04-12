e06fe8b22883403e9bd08bc2b6b8a04a9796a57b
Zhaokai <1986743385@qq.com>
2024-04-12 05:06:34 +0000

    删除文件 plugins

--- plugins/vista.lua
+++
@@ -1,6 +0,0 @@
--- 移动操作
---vim.api.nvim_set_keymap("n", "<C-l>", ":Vista<cr>", opts)
-return {
-    { "liuchengxu/vista.vim", event = "BufEnter" },
-    { "universal-ctags/ctags", event = "BufEnter" },
-}
