de5ab477ad0b2d4528727c9d8df8adea2032a793
Zhaokai <1986743385@qq.com>
2024-04-12 05:07:33 +0000

    删除文件 polish.lua

--- polish.lua
+++
@@ -1,7 +0,0 @@
--- This function is run last and is a good place to configuring
--- augroups/autocommands and custom filetypes also this just pure lua so
--- anything that doesn't fit in the normal config locations above can go here
-return function()
-  -- 配置文件入口,主要负责加载其他配置文件
-  require("user/autocmd")
-end
